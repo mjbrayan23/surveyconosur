a670fd23d23e6f1dc3633427afba496c7303b751
mjbrayan23 <brayan@rpa.com.mx>
2025-02-14 15:11:24 -0600

    Corrección: cambio de puerto dinámico y creación de logs

--- src/server.js
+++ src/server.js
@@ -90,7 +90,7 @@ process.on("unhandledRejection", (reason, promise) => {
 
 // 📌 Iniciar el servidor
 // Definir el puerto (Usa el puerto que Plesk asigna)
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 0;
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`🚀 Servidor corriendo en el puerto ${PORT}`);
 
